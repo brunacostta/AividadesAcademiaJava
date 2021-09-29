5e7e07fecdce605bb03c72202c5957a37155864f
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-09-29 15:04:22 -0300

    Criação segunda pessoa e sysout

--- Java2_poo_atp15/Principal.java
+++ Java2_poo_atp15/Principal.java
@@ -10,6 +10,14 @@ public class Principal {
         p1.nome = "Bruna";
         p1.sobrenome = "Bittencourt";
         p1.idade = 31;
+
+        Pessoa p2 = new Pessoa();
+        p2.nome = "Thiago";
+        p2.sobrenome = "Bittencourt";
+        p2.idade = 35;
+
+        System.out.printf("%s %s %d", p1.nome, p1.sobrenome, p1.idade);
+        System.out.printf("\n%s %s %d", p2.nome, p2.sobrenome, p2.idade);
         
     }
     static void cabecalho(){
