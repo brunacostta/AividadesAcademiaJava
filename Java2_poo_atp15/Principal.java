783582c5b337d72f2a651102907966de7fa0f25b
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-09-29 15:29:46 -0300

    Criação método mensagem

--- Java2_poo_atp15/Principal.java
+++ Java2_poo_atp15/Principal.java
@@ -16,11 +16,15 @@ public class Principal {
         p2.sobrenome = "Bittencourt";
         p2.idade = 35;
 
-        System.out.printf("%s %s %d", p1.nome, p1.sobrenome, p1.idade);
-        System.out.printf("\n%s %s %d", p2.nome, p2.sobrenome, p2.idade);
+        mensagem(p1.nome, p1.sobrenome, p1.idade);
+        mensagem(p2.nome, p2.sobrenome, p2.idade);
         
     }
     static void cabecalho(){
-        System.out.println("\n\t*************** Cadastro de Pessoa com classe ********************");
+        System.out.println("\n\t********* Cadastro de Pessoa com classe **************");
+    }
+
+    static void mensagem(String nome, String sobrenome, int idade){
+        System.out.printf("\n %s %s %d", nome, sobrenome, idade);
     }
 }
