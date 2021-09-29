3ed067ada0f85c662ade0e5d0e46c93d8e19dac5
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-09-29 14:48:24 -0300

    Criação da classe

---
+++ Java2_poo_atp15/Pessoa.java
@@ -0,0 +1,7 @@
+package Java2_poo_atp15;
+
+public class Pessoa{
+ String nome;
+ String sobrenome;
+ int idade; 
+}
---
+++ Java2_poo_atp15/Principal.java
@@ -0,0 +1,9 @@
+package Java2_poo_atp15;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
