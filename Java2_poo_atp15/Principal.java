ca16c4695ffcf18ee328709aabcc1b8bba214b30
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-09-29 14:51:11 -0300

    Criação do cabeçalho

--- Java2_poo_atp15/Principal.java
+++ Java2_poo_atp15/Principal.java
@@ -3,7 +3,11 @@ package Java2_poo_atp15;
 public class Principal {
 
     public static void main(String[] args) {
+
+        cabecalho();
         
     }
-    
+    static void cabecalho(){
+        System.out.println("\n\t*************** Cadastro de Pessoa com classe ********************");
+    }
 }
