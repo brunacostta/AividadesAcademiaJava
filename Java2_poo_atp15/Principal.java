874af1c177fdbb0939ec9ba060065564f2c2ed18
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-09-29 17:55:09 -0300

    Melhorias

--- Java2_poo_atp15/Principal.java
+++ Java2_poo_atp15/Principal.java
@@ -15,16 +15,19 @@ public class Principal {
         p2.nome = "Thiago";
         p2.sobrenome = "Bittencourt";
         p2.idade = 35;
-
+        
+        /* Primeira resolução, antes da explicação do profº.
         mensagem("A primeira pessoa é: " + p1.nome, p1.sobrenome, p1.idade);
-        mensagem("A segunda pessoa é: " + p2.nome, p2.sobrenome, p2.idade);
+        mensagem("A segunda pessoa é: " + p2.nome, p2.sobrenome, p2.idade);*/
+
+        p1.imprimir();
+        p2.imprimir();
         
     }
     static void cabecalho(){
         System.out.println("\n\t********* Cadastro de Pessoa com classe **************");
     }
-
-    static void mensagem(String nome, String sobrenome, int idade){
+        /*static void mensagem(String nome, String sobrenome, int idade){
         System.out.printf("\n%s %s %d anos.", nome, sobrenome, idade);
-    }
+    }*/
 }
