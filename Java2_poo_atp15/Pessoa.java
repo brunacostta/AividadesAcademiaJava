58e9c07cc2cec5208cf990162a6ed32f851dd44f
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-09-30 11:32:43 -0300

    atualização

--- Java2_poo_atp15/Pessoa.java
+++ Java2_poo_atp15/Pessoa.java
@@ -1,7 +1,12 @@
 package Java2_poo_atp15;
 
 public class Pessoa{
- String nome;
- String sobrenome;
- int idade; 
+  String nome;
+  String sobrenome;
+  int idade; 
+
+  public void imprimir(){
+    System.out.printf("\n%s %s %d anos.", nome, sobrenome, idade);
+  }
+
 }
