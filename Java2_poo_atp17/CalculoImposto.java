c6e5c955529faa86667c0c0aa668abf0e9caa53f
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-01 14:57:45 -0300

    Criação das variáveis

--- Java2_poo_atp17/CalculoImposto.java
+++ Java2_poo_atp17/CalculoImposto.java
@@ -1,7 +1,10 @@
 package Java2_poo_atp17;
 
 public class CalculoImposto {
-    
+
+    private double iss = 5.0;
+    private double iof = 4.08;
+    private double ir = 7.5;
     
     public static void calculoIss() {
         
