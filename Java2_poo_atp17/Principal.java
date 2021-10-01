a8e246f99f2ab7e8e571c33bc065dafd8a68829f
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-01 15:09:42 -0300

    Criação do cabeçalho

--- Java2_poo_atp17/Principal.java
+++ Java2_poo_atp17/Principal.java
@@ -2,6 +2,14 @@ package Java2_poo_atp17;
 
 public class Principal {
     public static void main(String[] args) {
+
+        cabecalho();
+        
+    }
+
+    static void cabecalho() {
+        System.out.println("\n\t------------- Calculadora de Impostos ---------------");
+        System.out.println("\t=====================================================");
         
     }
     
