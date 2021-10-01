ca68e2855596c07aa409a94c5f1365472e37e4bb
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-01 15:09:26 -0300

    Criação dos parâmentros

--- Java2_poo_atp17/CalculoImposto.java
+++ Java2_poo_atp17/CalculoImposto.java
@@ -2,19 +2,24 @@ package Java2_poo_atp17;
 
 public class CalculoImposto {
 
-    private double iss = 5.0;
-    private double iof = 4.08;
-    private double ir = 7.5;
+    private double taxaIss = 5.0;
+    private double taxaIof = 4.08;
+    private double taxaIr = 7.5;
     
-    public static void calculoIss() {
+    public double calculoIss(double salario) {
+        double resultadoIss = salario * taxaIss;
+        return resultadoIss;
         
     }
 
-    public static void calculoIof() {
+    public double calculoIof(double salario) {
+        double resultadoIof = salario * taxaIof;
+        return resultadoIof;
         
     }
 
-    public static void calculoIr() {
-        
+    public double calculoIr(double salario) {
+        double resultadoIr = salario * taxaIr;
+        return resultadoIr;
     }
 }
