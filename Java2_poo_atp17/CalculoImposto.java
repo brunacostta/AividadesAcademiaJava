9a57d52d0bcf6a4ad15e2a16a1827228b6f3dba2
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-01 14:49:58 -0300

    Criação dos métodos calculo ISS, IOF e IR

--- Java2_poo_atp17/CalculoImposto.java
+++ Java2_poo_atp17/CalculoImposto.java
@@ -2,4 +2,16 @@ package Java2_poo_atp17;
 
 public class CalculoImposto {
     
+    
+    public static void calculoIss() {
+        
+    }
+
+    public static void calculoIof() {
+        
+    }
+
+    public static void calculoIr() {
+        
+    }
 }
--- Java2_poo_atp17/Principal.java
+++ Java2_poo_atp17/Principal.java
@@ -1,5 +1,8 @@
 package Java2_poo_atp17;
 
 public class Principal {
+    public static void main(String[] args) {
+        
+    }
     
 }
