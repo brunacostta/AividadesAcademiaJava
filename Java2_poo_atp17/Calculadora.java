36062ea3d965177d9389515a302d486e0b721285
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-01 15:35:58 -0300

    Correção do IOF

--- Java2_poo_atp17/Calculadora.java
+++ Java2_poo_atp17/Calculadora.java
@@ -2,7 +2,6 @@ package Java2_poo_atp17;
 
 public class Calculadora {
     public static void main(String[] args) {
-
         cabecalho();
 
         CalculoImposto cc1 = new CalculoImposto();
@@ -10,7 +9,7 @@ public class Calculadora {
         double taxaIof = cc1.calculoIof(1800.00);
         double taxaIr = cc1.calculoIr(1800.00);
         
-        System.out.printf("\nO valor do ISS que será descontado é de: R$ %.2f ", taxaIss);
+        System.out.printf("O valor do ISS que será descontado é de: R$ %.2f ", taxaIss);
         System.out.printf("\nO valor do IOF que será descontado é de: R$ %.2f ", taxaIof);
         System.out.printf("\nO valor do IR que será descontado é de: R$ %.2f ", taxaIr);
     }
@@ -18,7 +17,5 @@ public class Calculadora {
     static void cabecalho() {
         System.out.println("\n\t------------- Calculadora de Impostos ---------------");
         System.out.println("\t=====================================================");
-        
-    }
-    
+    } 
 }
--- Java2_poo_atp17/CalculoImposto.java
+++ Java2_poo_atp17/CalculoImposto.java
@@ -3,19 +3,17 @@ package Java2_poo_atp17;
 public class CalculoImposto {
 
     private double taxaIss = 0.05;
-    private double taxaIof = 0.408;
+    private double taxaIof = 0.0408;
     private double taxaIr = 0.075;
     
     public double calculoIss(double salario) {
         double resultadoIss = salario * taxaIss;
         return resultadoIss;
-        
     }
 
     public double calculoIof(double salario) {
         double resultadoIof = salario * taxaIof;
         return resultadoIof;
-        
     }
 
     public double calculoIr(double salario) {
