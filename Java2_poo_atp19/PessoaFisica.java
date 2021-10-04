b95cfdb37b3a89615b63a1cff39f28280e60d5d8
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-04 17:34:20 -0300

    Criado get e set de cada atributo

--- Java2_poo_atp19/PessoaFisica.java
+++ Java2_poo_atp19/PessoaFisica.java
@@ -7,5 +7,53 @@ public class PessoaFisica {
     private int idade;
     private String rg;
     private String cpf;
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
     
 }
