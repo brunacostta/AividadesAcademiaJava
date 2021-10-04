e2ade3957bfdf2a674d5afecbc91f5b19fa6d141
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-04 17:31:54 -0300

    Criada classe Pessoa Fisica

---
+++ Java2_poo_atp19/PessoaFisica.java
@@ -0,0 +1,11 @@
+package Java2_poo_atp19;
+
+public class PessoaFisica {
+    private int codigo;
+    private String nome;
+    private String sobrenome;
+    private int idade;
+    private String rg;
+    private String cpf;
+    
+}
