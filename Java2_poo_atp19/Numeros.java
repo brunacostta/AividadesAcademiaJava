4ba36535b220ab5993ec07045f2c72dcb189b648
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-04 17:26:53 -0300

    Criado array de numeros inteiros com 10 posições e adicionado um valor para cada

--- Java2_poo_atp19/Numeros.java
+++ Java2_poo_atp19/Numeros.java
@@ -2,7 +2,20 @@ package Java2_poo_atp19;
 
 public class Numeros {
     public static void main(String[] args) {
-        
+        int[] numeros = new int[10];
+
+        numeros[0] = 10;
+        numeros[1] = 20;
+        numeros[2] = 30;
+        numeros[3] = 40;
+        numeros[4] = 50;
+        numeros[5] = 60;
+        numeros[6] = 70;
+        numeros[7] = 80;
+        numeros[8] = 90;
+        numeros[9] = 100;
+
+
     }
     
 }
