d87a877f31bf469ba5f04c5e954d5ac244415781
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-04 13:25:36 -0300

    Criação get e set Conta Corrente

--- Java2_poo_atp18/ContaCorrente.java
+++ Java2_poo_atp18/ContaCorrente.java
@@ -6,6 +6,37 @@ public class ContaCorrente {
     private int numero;
     private int codigo_cliente;
     private double saldo;
-    
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setCodigo_cliente(int codigo_cliente) {
+        this.codigo_cliente = codigo_cliente;
+    }
+
+    public int getCodigo_cliente() {
+        return codigo_cliente;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
     
 }
