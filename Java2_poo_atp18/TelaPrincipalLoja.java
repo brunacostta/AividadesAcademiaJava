535a3be8fffef7312294eef7ae63929d9534375a
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-04 13:33:33 -0300

    Craido objeto do Produto, chamada de todos osm métodos set e get e impressão.

--- Java2_poo_atp18/TelaPrincipalLoja.java
+++ Java2_poo_atp18/TelaPrincipalLoja.java
@@ -3,7 +3,20 @@ package Java2_poo_atp18;
 public class TelaPrincipalLoja {
 
     public static void main(String[] args) {
-        
+        Produto p1 = new Produto();
+
+        p1.setCodigo(10);
+        System.out.println(p1.getCodigo());
+
+        p1.setNome("Bruna");
+        System.out.println(p1.getNome());
+
+        p1.setDescricao("Hidratante para as mãos");
+        System.out.println(p1.getDescricao());
+
+        p1.setValor(19.99);
+        System.out.println(p1.getValor());
+
     }
     
 }
