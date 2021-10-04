306d1e592997f8e415f97335f98029ff7301f1f8
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-04 13:23:58 -0300

    Criação da classe Conta Corrente e seus atributos

---
+++ Java2_poo_atp18/ContaCorrente.java
@@ -0,0 +1,11 @@
+package Java2_poo_atp18;
+
+public class ContaCorrente {
+
+    private int agencia;
+    private int numero;
+    private int codigo_cliente;
+    private double saldo;
+    
+    
+}
