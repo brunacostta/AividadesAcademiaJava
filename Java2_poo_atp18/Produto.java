212c9a808f60887053bfa12170f65bda3dab4ab8
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-04 13:21:16 -0300

    Criação da classe produto e do get e set dos atributos

---
+++ Java2_poo_atp18/Produto.java
@@ -0,0 +1,43 @@
+package Java2_poo_atp18;
+
+public class Produto {
+
+    private int codigo;
+    private String nome;
+    private String descricao;
+    private double valor;
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+    
+}
+
