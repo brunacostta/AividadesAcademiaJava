7656c248784490f5df732aba9bfde2d7cd0d071b
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-05 15:38:00 -0300

    Criado get e set dos métodos

--- Java2_poo_atp20/Aluno.java
+++ Java2_poo_atp20/Aluno.java
@@ -4,5 +4,21 @@ public class Aluno extends Pessoa{
 
     private String curso;
     private String turma;
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
     
 }
--- Java2_poo_atp20/Funcionario.java
+++ Java2_poo_atp20/Funcionario.java
@@ -3,5 +3,21 @@ package Java2_poo_atp20;
 public class Funcionario extends Pessoa{
 
     private double salario;
-    private String departamento;    
+    private String departamento; 
+    
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
 }
