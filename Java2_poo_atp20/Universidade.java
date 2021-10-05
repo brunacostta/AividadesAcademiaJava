bcef790f3b2896f6a2949f013c066332ffafa8ca
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-05 15:45:47 -0300

    Utilizado método set para incluir valores nos objetos

--- Java2_poo_atp20/Universidade.java
+++ Java2_poo_atp20/Universidade.java
@@ -4,10 +4,16 @@ public class Universidade {
     public static void main(String[] args) {
 
         Funcionario f1 = new Funcionario();
+        f1.setSalario(9654.00);
+        f1.setDepartamento("Secretaria");
 
         Coordenador c1 = new Coordenador();
+        c1.setCurso_coordenado("Analise e Desenvolvimento");
+        c1.setBonus(2000.00);
 
         Aluno a1 = new Aluno();
+        a1.setCurso("Engenharia");
+        a1.setTurma("1DEN");
         
     }
 }
