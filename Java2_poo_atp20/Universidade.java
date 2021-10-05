3de5f10608dd11c076237c849b73c8b8d9f7d001
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-05 15:40:41 -0300

    Criada classe universidade e objetos.

---
+++ Java2_poo_atp20/Universidade.java
@@ -0,0 +1,13 @@
+package Java2_poo_atp20;
+
+public class Universidade {
+    public static void main(String[] args) {
+
+        Funcionario f1 = new Funcionario();
+
+        Coordenador c1 = new Coordenador();
+
+        Aluno a1 = new Aluno();
+        
+    }
+}
