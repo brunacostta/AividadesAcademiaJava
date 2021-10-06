7cf9e1db6fc0819350bf475ddcdbd103d7811a23
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-06 13:44:51 -0300

    Criada classe teste e objetos

---
+++ Java2_poo_atp21/Teste.java
@@ -0,0 +1,11 @@
+package Java2_poo_atp21;
+
+public class Teste {
+    public static void main(String[] args) {
+        
+        Conta c1 = new Conta();
+
+        ContaCorrente cc1 = new ContaCorrente();
+    }
+    
+}
