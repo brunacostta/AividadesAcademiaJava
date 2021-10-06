acc5906c2bca67ef07dbf14d518d5842bcd16076
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-06 13:33:14 -0300

    Criado get e set dos atributos da classe Conta

--- Java2_poo_atp21/Conta.java
+++ Java2_poo_atp21/Conta.java
@@ -4,5 +4,21 @@ public class Conta {
 
     private double saldo;
     private int codigo_cliente;
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setCodigo_cliente(int codigo_cliente) {
+        this.codigo_cliente = codigo_cliente;
+    }
+
+    public int getCodigo_cliente() {
+        return codigo_cliente;
+    }
     
 }
