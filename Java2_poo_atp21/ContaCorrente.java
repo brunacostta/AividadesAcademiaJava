afa13f09efdfaf59faf5082095681172430096e0
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-06 13:35:17 -0300

    Criada classe Conta Corrente e seus atributos privados

---
+++ Java2_poo_atp21/ContaCorrente.java
@@ -0,0 +1,9 @@
+package Java2_poo_atp21;
+
+public class ContaCorrente {
+
+    private double taxa_transferencia;
+    private double taxa_manutencao;
+    private int numero_transferencia;
+    
+}
