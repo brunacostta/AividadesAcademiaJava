cbb9d03ad456eae60e2c3573229c232df1ebc9bb
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-06 13:47:20 -0300

    Inserido set em cada objeto

--- Java2_poo_atp21/Teste.java
+++ Java2_poo_atp21/Teste.java
@@ -4,8 +4,12 @@ public class Teste {
     public static void main(String[] args) {
         
         Conta c1 = new Conta();
+        c1.setCodigo_cliente(12);
+        c1.setSaldo(8000.00);
 
         ContaCorrente cc1 = new ContaCorrente();
+        cc1.setCodigo_cliente(12);
+        cc1.setSaldo(12000.00);
     }
     
 }
