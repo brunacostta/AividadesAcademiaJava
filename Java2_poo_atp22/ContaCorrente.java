c1fa2a9546cf014836b8004dac535f970810d866
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-07 16:28:44 -0300

    Criada Tela Banco

--- Java2_poo_atp22/ContaCorrente.java
+++ Java2_poo_atp22/ContaCorrente.java
@@ -2,7 +2,22 @@ package Java2_poo_atp22;
 
 public class ContaCorrente {
     private double saldo;
-    private double taxa_cliente;
+    private double taxa_saque = 5.0;
     private int codigo_cliente;
     
+    public ContaCorrente(double saldo, int codigo_cliente) {
+        this.saldo = saldo;
+        this.codigo_cliente = codigo_cliente;
+    }
+
+    public double saque(double saque) {
+        this.saldo -= saque;
+        double saldo_saque = this.saldo - this.taxa_saque;
+        return saldo_saque;
+    }
+
+    public double deposito(double deposito) {
+        double saldo_deposito = deposito + this.saldo;
+        return saldo_deposito;        
+    }
 }
---
+++ Java2_poo_atp22/TelaBanco.java
@@ -0,0 +1,8 @@
+package Java2_poo_atp22;
+
+public class TelaBanco {
+    public static void main(String[] args) {
+        
+    }
+    
+}
