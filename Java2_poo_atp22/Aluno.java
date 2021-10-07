faff026100a5fdd24654b06c8ed1b77294c9acb6
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-07 15:59:22 -0300

    Criado get e set para cada atributo

--- Java2_poo_atp22/Aluno.java
+++ Java2_poo_atp22/Aluno.java
@@ -4,5 +4,29 @@ public class Aluno {
     private String nome_completo;
     private int idade;
     private int matricula;
+
+    public void setNome_completo(String nome_completo) {
+        this.nome_completo = nome_completo;
+    }
+
+    public String getNome_completo() {
+        return nome_completo;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
     
 }
