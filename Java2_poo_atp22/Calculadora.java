e7d650f10e567969a2ef05d1ffd259f559d0da7a
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-07 16:51:38 -0300

    Criado método que retorna 3 floats

--- Java2_poo_atp22/Calculadora.java
+++ Java2_poo_atp22/Calculadora.java
@@ -8,4 +8,8 @@ public class Calculadora {
     public double soma(double d1, double d2){
         return d1 + d2;
     }
+
+    public float soma(float f1, float f2, float f3) {
+        return f1 + f2 + f3;
+    }
 }
