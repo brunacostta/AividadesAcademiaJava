edb8f5a455418f56e12ae9a952ef38cd213d2e6c
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-07 16:13:27 -0300

    Criado objetos da classe Aluno na Tela, inserido valores com setters e impressão com toString

--- Java2_poo_atp22/Aluno.java
+++ Java2_poo_atp22/Aluno.java
@@ -6,33 +6,34 @@ public class Aluno {
     private int matricula;
 
     
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-
+    
     public void setNome_completo(String nome_completo) {
         this.nome_completo = nome_completo;
     }
-
+    
     public String getNome_completo() {
         return nome_completo;
     }
-
+    
     public void setIdade(int idade) {
         this.idade = idade;
     }
-
+    
     public int getIdade() {
         return idade;
     }
-
+    
     public void setMatricula(int matricula) {
         this.matricula = matricula;
     }
-
+    
     public int getMatricula() {
         return matricula;
     }
-   
+    
+    @Override
+    public String toString() {
+        String dado_aluno = this.nome_completo + "\n" + this.idade + "\n" + this.matricula;
+        return dado_aluno;
+    }
 }
--- Java2_poo_atp22/Tela.java
+++ Java2_poo_atp22/Tela.java
@@ -3,6 +3,11 @@ package Java2_poo_atp22;
 public class Tela {
     public static void main(String[] args) {
         
+        Aluno a1 = new Aluno();
+        a1.setMatricula(33);
+        a1.setNome_completo("Ana Maria Prado");
+        a1.setIdade(17);
+
+        System.out.println(a1);
     }
-    
 }
