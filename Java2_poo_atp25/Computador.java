2128f86f8355329c45669f774a39bf5fea1ffa3e
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-18 16:36:40 -0300

    Criada classe Computador e Notebook

---
+++ Java2_poo_atp25/Computador.java
@@ -0,0 +1,10 @@
+//Crie uma classe computador com atributos públicos: marca, memoria ram, processador e disco rigido.
+
+package Java2_poo_atp25;
+
+public class Computador {
+    public String marca;
+    public String memoria;
+    public String processador;
+    public int disco_rigido;    
+}
---
+++ Java2_poo_atp25/Notebook.java
@@ -0,0 +1,10 @@
+//Crie uma classe notebook que herda de computador com 
+//os atributos públicos: bateria, velocidade wifi e versão bluetooth.
+
+package Java2_poo_atp25;
+
+public class Notebook extends Computador {
+    public int bateria;
+    public int velocidade_wifi;
+    public double versão_bluetooth;
+}
