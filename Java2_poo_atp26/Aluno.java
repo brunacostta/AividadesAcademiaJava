92c16c2e10c57761c4cbf204a3deff8191e41d1a
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-19 14:20:24 -0300

    Iniciada atividade 26

---
+++ Java2_poo_atp26/Aluno.java
@@ -0,0 +1,9 @@
+//Crie uma classe Aluno que herda de pessoa e que possua os atributos públicos: matricula, turma e curso.
+
+package Java2_poo_atp26;
+
+public class Aluno extends Pessoa{
+    public String matricula;
+    public String turma;
+    public String curso;
+}
---
+++ Java2_poo_atp26/Pessoa.java
@@ -0,0 +1,9 @@
+//Crie uma classe Pessoa com os atributos públicos: nome, sobrenome e idade.
+
+package Java2_poo_atp26;
+
+public class Pessoa {
+    public String nome;
+    public String sobrenome;
+    public byte idade;
+}
