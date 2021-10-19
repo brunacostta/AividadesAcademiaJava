05273940b345ad1080a6414612093aa9915b997c
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-19 15:19:52 -0300

    Criada uma classe Main com um método main.

---
+++ Java2_poo_atp26/Dados.java
@@ -0,0 +1,70 @@
+//Crie uma classe Dados com um atributo privado do tipo array de Object.
+
+package Java2_poo_atp26;
+
+import java.rmi.server.ObjID;
+
+//Crie uma classe Dados com um atributo privado do tipo array de Object.
+public class Dados {
+    private Object[] dados;
+    int posicao;
+    int tamanho = 5;
+
+    //Crie uma inicialização do array para 5 posições dentro do do método construtor da classe
+    public Dados() {
+       this.dados = new Object[5];
+    }
+
+    //Crie um método para mostrar quantos elementos já foram adicionados ao array.
+    public int size(){
+        return tamanho +1;
+    }
+
+    //Crie um método para adicionar elementos ao array e que permita receber tanto objetos da classe pessoa quanto objetos da classe Aluno. Caso o array já tenha todas as posições ocupadas, deve ser adicionado mais 5 posições ao array.
+    public String add(Object obj) {
+        String mensagem;
+
+        if (posicao < dados.length) {
+            dados[posicao] = obj;
+            posicao ++;
+        } else {
+            tamanho = tamanho * 2;
+            Object[] dados2 = new Object[tamanho];
+
+            for (int i = 0; i < dados2.length; i++) {
+                dados2[i] = dados[i];
+            }
+            dados = dados2;
+        }
+    mensagem = "Linha adicionada";
+    return mensagem;
+    }
+
+    //Crie um método para remover um elemento do array.
+    public String remove(Object obj){
+        for (int i = 0; i < dados.length; i++) {
+            if( dados[i].equals(obj)){
+            reorganiza(i);
+            posicao --;
+            return "removido com sucesso";
+            }
+        }
+        return "Não encontrado";
+    }
+
+    public void reorganiza(int posicao){
+    for (int i = posicao; i < dados.length; i++) {
+        this.dados[i] = this.dados[i+1];
+        }
+    }
+
+    //Crie um método para verificar se um determinado objeto já foi salvo no array.
+    public boolean contains(Object obj){
+        for (int i = 0; i < posicao; i++) {
+            if(dados[i].equals(obj)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Java2_poo_atp26/Main.java
@@ -0,0 +1,8 @@
+package Java2_poo_atp26;
+
+//Crie uma classe Main com um método main.
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
