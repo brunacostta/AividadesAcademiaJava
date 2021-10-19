8930508f55afa6c210ebcf0689acebdefec42cac
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-10-19 14:47:34 -0300

    Criada uma sobrescrita do método equals nas duas classes para comparar os dados especificos de cada uma.

--- Java2_poo_atp26/Aluno.java
+++ Java2_poo_atp26/Aluno.java
@@ -6,4 +6,16 @@ public class Aluno extends Pessoa{
     public String matricula;
     public String turma;
     public String curso;
+
+    //Crie uma sobrescrita do método equals nas duas classes para comparar os dados especificos de cada uma.
+    @Override
+    public boolean equals(Object obj) {
+        Aluno outro_aluno = (Aluno)obj;
+
+        if (this.matricula.equals(outro_aluno.matricula) && this.turma.equals(outro_aluno.turma) && this.curso.equals(outro_aluno.curso)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- Java2_poo_atp26/Pessoa.java
+++ Java2_poo_atp26/Pessoa.java
@@ -5,5 +5,17 @@ package Java2_poo_atp26;
 public class Pessoa {
     public String nome;
     public String sobrenome;
-    public byte idade;
+    public int idade;
+
+    //Crie uma sobrescrita do método equals nas duas classes para comparar os dados especificos de cada uma.
+    @Override
+    public boolean equals(Object obj) {
+        Pessoa outra_pessoa = (Pessoa)obj;
+      
+        if (this.nome.equals(outra_pessoa.nome) && this.sobrenome.equals(outra_pessoa.sobrenome) && this.idade == outra_pessoa.idade) {
+            return true;
+        } else {
+            return false;
+        }  
+    }
 }
